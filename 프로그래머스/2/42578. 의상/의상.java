a3d3b9ba4d9fcd07462ed14eb2e24fdee3e8b263
박나현 <am3327@naver.com>
2026-03-18 17:12:00 +0900

    [level 2] Title: 의상, Time: 0.11 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -4,15 +4,14 @@ class Solution {
     public int solution(String[][] clothes) {
         int answer = 1;
         
-        Map<String, List<String>> map = new HashMap<>();
+        Map<String, Integer> map = new HashMap<>();
         
         for (String[] cloth : clothes) {
-            map.putIfAbsent(cloth[1], new ArrayList<>());
-            map.get(cloth[1]).add(cloth[0]);
+            map.put(cloth[1], map.getOrDefault(cloth[1], 0) + 1);
         }
         
-        for (String type : map.keySet()) {
-            answer = answer * (map.get(type).size() + 1);
+        for (int v : map.values()) {
+            answer = answer * (v + 1);
         }        
         
         return answer - 1;
