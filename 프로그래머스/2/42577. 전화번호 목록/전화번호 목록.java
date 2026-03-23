88f695c7f351ce2781893cec46ed1c12c8b69499
박나현 <am3327@naver.com>
2026-03-23 15:44:23 +0900

    [level 2] Title: 전화번호 목록, Time: 375.20 ms, Memory: 98.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book);
+        
+        for (int i = 0; i < phone_book.length-1; i++) {
+            // for (int j = i+1; j < phone_book.length; j++) {
+                if (phone_book[i+1].startsWith(phone_book[i])) {
+                    return false;
+                }
+            // }
+        }
+        
+        return true;
+    }
+}
