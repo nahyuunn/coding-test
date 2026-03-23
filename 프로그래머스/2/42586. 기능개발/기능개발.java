e51cf2a05ddcbb6e2d28c78a7709651d4c02e7ff
박나현 <am3327@naver.com>
2026-03-23 22:19:59 +0900

    [level 2] Title: 기능개발, Time: 0.07 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,24 +1,32 @@
 import java.util.*;
 
 class Solution {
-    public Queue<Integer> solution(int[] progresses, int[] speeds) {
-        ArrayDeque<Integer> answer = new ArrayDeque<>();
+    public int[] solution(int[] progresses, int[] speeds) {
+        int pl = progresses.length;
+        int[] workDays = new int[pl];
+        List<Integer> ans = new ArrayList<>();
 
-        int length = progresses.length;
-        int current = -1;
+        for (int i = 0; i < pl; i++) {
+            int n = 100 - progresses[i];
+            workDays[i] = n % speeds[i] == 0 ? n / speeds[i] : n / speeds[i] + 1;
+        }
         
-        for (int i = 0 ; i < length ; i++) {
-            int rest = 100 - progresses[i];
-            int day = rest / speeds[i];
-            
-            if (rest % speeds[i] != 0) day++;
-            
-            if (day > current) {
-                answer.offerLast(1);
-                current = day;
-            } else {
-                answer.offerLast(answer.pollLast() + 1);
+        for (int i = 0; i < pl; i++) {
+            int count = 1;
+
+            for (int j = i+1; j <= pl; j++) {
+                if (j == pl || workDays[i] < workDays[j]) {
+                    ans.add(count);
+                    i = j-1;
+                    break;
+                }
+                else count++;
             }
+        }
+        int[] answer = new int[ans.size()];
+        int idx = 0;
+        for (int a : ans) {
+            answer[idx++] = a;
         }
         return answer;
     }
