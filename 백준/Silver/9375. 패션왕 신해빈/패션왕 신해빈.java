41cad0c93025732aa0b6019b1b815a9fbf927f93
박나현 <am3327@naver.com>
2026-03-25 16:15:55 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 104 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -2,28 +2,37 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static Map<String, Integer> map = new HashMap<>();
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
         int tc = Integer.parseInt(br.readLine());
-        
-        while (tc-- > 0) {
-            Map<String, Integer> map = new HashMap<>();
-            int n = Integer.parseInt(br.readLine());
-            while (n-- > 0) {
-                StringTokenizer st = new StringTokenizer(br.readLine());
-                st.nextToken();
-                String kind = st.nextToken();
-                map.put(kind, map.getOrDefault(kind, 0) + 1);
-            }
 
-            int answer = 1;
-            for (Map.Entry<String, Integer> entry : map.entrySet()) {
-                answer *= entry.getValue() + 1;
-            }
-            sb.append(answer-1).append("\n");
+        while (tc-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            makeKindMap(n);
+            getAnswer();
         }
         System.out.println(sb);
+    }
+    
+    static void makeKindMap(int n)  throws IOException {
+        map = new HashMap<>();
+        while (n-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            st.nextToken();
+            String kind = st.nextToken();
+            map.put(kind, map.getOrDefault(kind, 0) + 1);
+        }
+    }
+    
+    static void getAnswer() {
+        int answer = 1;
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            answer *= entry.getValue() + 1;
+        }
+        sb.append(answer-1).append("\n");
     }
 }
