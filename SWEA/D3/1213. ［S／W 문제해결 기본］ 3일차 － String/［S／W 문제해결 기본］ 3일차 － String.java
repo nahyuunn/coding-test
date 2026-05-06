200e2b931374bcb5e47feaef7965867b7dd40ba6
박나현 <am3327@naver.com>
2026-05-06 17:32:22 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - String, Time: 89 ms, Memory: 26,880 KB -BaekjoonHub

---
+++ SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+public class Solution {
+    static int TC = 10;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        while (TC-- > 0) {
+            int count = 0;
+
+            int num = Integer.parseInt(br.readLine());
+            String target = br.readLine();
+            String str = br.readLine();
+
+            char[] charArr = str.toCharArray();
+
+            for (int i = 0; i < str.length() - target.length() + 1; i++) {
+                StringBuilder now = new StringBuilder();
+                int nowIdx = i;
+
+                for (int j = 0; j < target.length(); j++) {
+                    now.append(charArr[nowIdx++]);
+                }
+
+                if (now.toString().equals(target)) {
+                    count++;
+                }
+            }
+            sb.append("#").append(num).append(" ").append(count).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
