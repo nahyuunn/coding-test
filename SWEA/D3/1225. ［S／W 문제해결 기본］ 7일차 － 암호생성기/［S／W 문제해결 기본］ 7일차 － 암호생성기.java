9ffb8eb3c26f5c16ba5ae05e34e7b87d42e56ead
박나현 <am3327@naver.com>
2026-05-11 15:47:34 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 99 ms, Memory: 27,776 KB -BaekjoonHub

--- SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
@@ -2,43 +2,43 @@ import java.util.*;
 import java.io.*;
 
 public class Solution {
+    static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
         for (int tc = 0; tc < 10; tc++) {
             int tn = Integer.parseInt(br.readLine());
             Queue<Integer> q = new ArrayDeque<>();
             StringTokenizer st = new StringTokenizer(br.readLine());
-
             for (int i = 0; i < 8; i++) {
                 q.offer(Integer.parseInt(st.nextToken()));
             }
-
-            boolean flag = true;
-            while(flag) {
-                for (int t = 1; t <= 5; t++) {
-                    int cur = q.poll() - t;
-                    if (cur > 0) {
-                        q.offer(cur);
-                    } else {
-                        q.offer(0);
-                        flag = false;
-                        break;
-                    }
-                }
-            }
-
-            sb.append("#").append(tn).append(" ");
-
-            while(!q.isEmpty()) {
-                sb.append(q.poll());
-                if (!q.isEmpty()) sb.append(" ");
-            }
-            if (tc != 9) {
-                sb.append("\n");
-            }
+            encode(q);
+            printQ(tn, q, tc);
         }
         System.out.println(sb);
+    }
+
+    private static void encode(Queue<Integer> q) {
+        while(true) {
+            for (int t = 1; t <= 5; t++) {
+                int cur = q.poll() - t;
+                if (cur <= 0) {
+                    q.offer(0);
+                    return;
+                } else q.offer(cur);
+            }
+        }
+    }
+
+    private static void printQ(int tn, Queue<Integer> q, int tc) {
+        sb.append("#").append(tn).append(" ");
+        while(!q.isEmpty()) {
+            sb.append(q.poll());
+            if (!q.isEmpty()) sb.append(" ");
+        }
+        if (tc != 9) {
+            sb.append("\n");
+        }
     }
 }
