7f7b25e164afd7ba39be5093590b9a1354531710
박나현 <am3327@naver.com>
2026-05-12 16:42:16 +0900

    [D3] Title: 의석이의 세로로 말해요, Time: 76 ms, Memory: 24,832 KB -BaekjoonHub

--- SWEA/D3/5356. 의석이의 세로로 말해요/의석이의 세로로 말해요.java
+++ SWEA/D3/5356. 의석이의 세로로 말해요/의석이의 세로로 말해요.java
@@ -1,4 +1,3 @@
-import java.util.*;
 import java.io.*;
 
 public class Solution {
@@ -9,21 +8,19 @@ public class Solution {
         int tc = Integer.parseInt(br.readLine());
         for (int t = 1; t <= tc; t++) {
             String[] list = new String[5];
-
             int max = 0;
             for (int i = 0; i < 5; i++) {
                 list[i] = br.readLine();
                 max = Math.max(max, list[i].length());
             }
-            StringBuilder str = new StringBuilder();
+            sb.append("#").append(t).append(" ");
             for (int i = 0; i < max; i++) {
                 for (int j = 0; j < 5; j++) {
                     if (list[j].length() > i) {
-                        str.append(list[j].charAt(i));
+                        sb.append(list[j].charAt(i));
                     }
                 }
             }
-            sb.append("#").append(t).append(" ").append(str);
             if (t != tc) sb.append("\n");
         }
         System.out.print(sb);
