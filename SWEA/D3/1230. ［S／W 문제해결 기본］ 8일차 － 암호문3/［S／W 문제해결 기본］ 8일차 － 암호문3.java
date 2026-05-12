da9878322a4024f6f3442f72ff365ce38f70e960
박나현 <am3327@naver.com>
2026-05-12 14:50:14 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문3, Time: 131 ms, Memory: 35,072 KB -BaekjoonHub

---
+++ SWEA/D3/1230. ［S／W 문제해결 기본］ 8일차 － 암호문3/［S／W 문제해결 기본］ 8일차 － 암호문3.java
@@ -0,0 +1,58 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        for (int tc = 1; tc <= 10; tc++) {
+            int N = Integer.parseInt(br.readLine());
+
+            List<String> list = new ArrayList<>();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            while (N-- > 0) {
+                list.add(st.nextToken());
+            }
+
+            int fc = Integer.parseInt(br.readLine());
+            st = new StringTokenizer(br.readLine());
+
+            while (fc-- > 0) {
+                String cmd = st.nextToken();
+
+                if (cmd.equals("I")) {
+                    int x = Integer.parseInt(st.nextToken());
+                    int y = Integer.parseInt(st.nextToken());
+
+                    while (y-- > 0) {
+                        list.add(x++, st.nextToken());
+                    }
+                }
+
+                else if (cmd.equals("D")) {
+                    int x = Integer.parseInt(st.nextToken());
+                    int y = Integer.parseInt(st.nextToken());
+
+                    while (y-- > 0) {
+                        list.remove(x);
+                    }
+                }
+                else if (cmd.equals("A")) {
+                    int y = Integer.parseInt(st.nextToken());
+
+                    while (y-- > 0) {
+                        list.add(st.nextToken());
+                    }
+                }
+            }
+            sb.append("#").append(tc).append(" ");
+            for (int i = 0; i < 10; i++) {
+                sb.append(list.get(i));
+                if (i != 9) sb.append(" ");
+            }
+            if (tc != 10) sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+}
