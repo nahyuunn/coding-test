86880d2b305265cd653779f547aa7f012cde97b4
박나현 <am3327@naver.com>
2026-05-13 17:35:19 +0900

    [D3] Title: 민석이의 과제 체크하기, Time: 152 ms, Memory: 35,432 KB -BaekjoonHub

---
+++ SWEA/D3/5431. 민석이의 과제 체크하기/민석이의 과제 체크하기.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int T = Integer.parseInt(br.readLine());
+		for (int t = 1; t <= T; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+
+			int N = Integer.parseInt(st.nextToken());
+			int K = Integer.parseInt(st.nextToken());
+			
+			Set<Integer> set = new TreeSet<>();
+			for (int n = 1; n <= N; n++) {
+				set.add(n);
+			}
+			
+			st = new StringTokenizer(br.readLine());
+			for (int k = 0; k < K; k++) {
+				set.remove(Integer.parseInt(st.nextToken()));
+			}
+            
+			sb.append("#").append(t).append(" ");
+		
+			int length = N - K;
+			for (int num: set) {
+				sb.append(num);
+				if (length-- > 0) sb.append(" ");
+			}
+			if (t != T) sb.append("\n");
+		}
+		System.out.print(sb);	
+	}
+}
