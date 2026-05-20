aa47d392f0b5c981fd0283cd87b5ffec081d6598
박나현 <am3327@naver.com>
2026-05-20 14:04:25 +0900

    [D3] Title: 햄버거 다이어트, Time: 153 ms, Memory: 26,624 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -18,29 +18,26 @@ public class Solution {
 				igd[n][0] = Integer.parseInt(st.nextToken());
 				igd[n][1] = Integer.parseInt(st.nextToken());
 			}
-            scr = 0;
-
-			for (int i = 1; i <= N; i++) {
-				dfs(i, 0, 0, 0, 0, igd, K);
-			}
+			
+			scr = 0;
+			dfs(0, 0, 0, igd, K);
 			
 			System.out.println("#" + t + " " + scr);
 		}
 	}
 	
-	private static void dfs(int cnt, int depth, int score, int sum, int i, int[][] igd, int K) {
-		if (depth == cnt) {
-			if (sum <= K) {
-				scr = Math.max(scr, score);
-			}
-			return;
+	private static void dfs(int score, int sum, int i, int[][] igd, int K) {	
+		if (sum <= K) {
+			scr = Math.max(scr, score);
 		}
+		
 		if (i == igd.length) {
 			return;
 		}
+		
 		if (sum + igd[i][1] <= K) {
-			dfs(cnt, depth+1, score + igd[i][0], sum + igd[i][1], i+1, igd, K);
+			dfs(score + igd[i][0], sum + igd[i][1], i+1, igd, K);
 		}
-		dfs(cnt, depth, score, sum, i+1, igd, K);
+		dfs(score, sum, i+1, igd, K);
 	}
 }
