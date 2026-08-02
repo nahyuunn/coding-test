d4beec1beb5cfc073ef4d431fa0048a2e3adfb19
박나현 <am3327@naver.com>
2026-08-02 16:46:37 +0900

    [D2] Title: 파리 퇴치, Time: 74 ms, Memory: 25,984 KB -BaekjoonHub

---
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int t = 1; t <= T; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			
+			int[][] arr = new int[N][N];
+			
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < N; j++) {
+					arr[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			
+			int max = 0;
+			int sum = 0;
+
+			for (int i = 0; i < N - M + 1; i++) {
+				for (int j = 0; j < N - M + 1; j++) {
+					sum = 0;
+					for (int k = 0; k < M; k++) {
+						for (int l = 0; l < M; l++) {
+							sum += arr[i + k][j + l];
+						}
+					}
+					max = Math.max(sum, max);
+				}
+			}
+			System.out.println("#" + t + " " + max);
+			
+		}
+	}
+
+}
