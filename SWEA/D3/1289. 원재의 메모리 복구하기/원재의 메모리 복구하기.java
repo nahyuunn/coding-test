c002b8a2b399cedc1dd421baf816f00a04db1d8a
박나현 <am3327@naver.com>
2026-08-09 21:55:51 +0900

    [D3] Title: 원재의 메모리 복구하기, Time: 70 ms, Memory: 25,600 KB -BaekjoonHub

--- SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.java
+++ SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.java
@@ -8,13 +8,14 @@ public class Solution {
 		int T = Integer.parseInt(br.readLine());
 		
 		for (int t = 1; t <= T; t++) {
-			char[] origin = br.readLine().toCharArray();
-			char target = '0';
+			String origin = br.readLine();
+			char cur = '0';
 			int count = 0;
-			for (char ch : origin) {
-				if (ch != target) {
+			for (int i = 0; i < origin.length(); i++) {
+				char ch = origin.charAt(i);
+				if (ch != cur) {
 					count++;
-					target = ch;
+					cur = ch;
 				}
 			}
 			System.out.println("#" + t + " " + count);
